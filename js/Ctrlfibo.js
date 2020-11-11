4ed0463baa613ec8a753a0e63add874599457f93
pruebas3312han <74284298+pruebas3312han@users.noreply.github.com>
2020-11-10 23:12:31 -0600

    Update Ctrlfibo.js

--- js/Ctrlfibo.js
+++ js/Ctrlfibo.js
@@ -1,11 +1,16 @@
-html {font-family: sans-serif; text-align: left}
-body {display: inline-block; text-align: left}
-label,legend {font-weight: bold}
-*:required, .obligatorio{border:thin solid blue}
-input [type=number], .numero {text-align: right}
-dfn {font-weight: bold}
-
-br + output, fieldset + output, footer, form em {font-size: small}
-
-fieldset p {margin:0}fieldset label, label * {font-weight: normal}
-.error {color:red}
+"use strict";
+function fibonacci(){
+    var valor = document.getElementById('dato').value;
+    var a=0,b=1,c=b+a,out="";
+    for (var i=0; i<valor-1; i++){
+        a=b;
+        b=c;
+        c=a+b;
+        out=out+"\n"+c;
+    document.getElementById('resultado').innerHTML=out;
+    }
+}
+function borrar(){
+    document.getElementById("resultado").value ="";
+    document.getElementById("dato").value = "";   
+}
