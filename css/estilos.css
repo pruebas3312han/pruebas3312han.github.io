2302dec90f6a76b0cc61d727f2ac52b3acc7eb9f
pruebas3312han <74284298+pruebas3312han@users.noreply.github.com>
2020-11-10 23:11:44 -0600

    Create estilos.css

---
+++ css/estilos.css
@@ -0,0 +1,11 @@
+html {font-family: sans-serif; text-align: left}
+body {display: inline-block; text-align: left}
+label,legend {font-weight: bold}
+*:required, .obligatorio{border:thin solid blue}
+input [type=number], .numero {text-align: right}
+dfn {font-weight: bold}
+
+br + output, fieldset + output, footer, form em {font-size: small}
+
+fieldset p {margin:0}fieldset label, label * {font-weight: normal}
+.error {color:red}
